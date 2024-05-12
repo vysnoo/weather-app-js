2dbef782d1901310f5635862ce7b69945fddf276
vysnoo <vishnukumaresan21@gmail.com>
2024-05-12 17:14:29 +0530

    js progress

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const apiKey = "543fea77fac294acb9689cfe8ac613c7";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+
+async function checkWheather() {
+  const response = await fetch(apiUrl);
+}
