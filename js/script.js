c53f50a92bf89a7960e8a8e28d12e5735e737820
vysnoo <vishnukumaresan21@gmail.com>
2024-05-13 13:54:58 +0530

    mod

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 const apiKey = "543fea77fac294acb9689cfe8ac613c7";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWheather() {
-  const response = await fetch(apiUrl);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWheather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await response.json();
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML =
+    Math.round(data.main.temp) + "&deg;c";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 }
+
+searchBtn.addEventListener("click", () => {
+  checkWheather(searchBox.value);
+});
+checkWheather();
